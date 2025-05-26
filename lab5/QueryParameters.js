dc2e9554b510b8b1f8debb39871272a54cf52143
anissavaughn <anissa.vaughnn@gmail.com>
2025-05-26 16:06:23 -0400

    save lab progress

---
+++ lab5/QueryParameters.js
@@ -0,0 +1,31 @@
+export default function QueryParameters(app) {
+    app.get("/lab5/calculator", (req, res) => {
+      const { a, b, operation } = req.query;
+      let result = 0;
+      switch (operation) {
+        case "add":
+          result = parseInt(a) + parseInt(b);
+          break;
+        case "subtract":
+          result = parseInt(a) - parseInt(b);
+          break;
+          // implement multiply and divide on your own
+
+        case "multiply":
+            result = parseInt(a) * parseInt(b);
+            break;
+
+        case "divide":
+            if (parseInt(b) !== 0) {
+              result = parseInt(a) / parseInt(b);
+            } else {
+              result = "Cannot divide by zero";
+            }
+            break;
+        default:
+          result = "Invalid operation";
+      }
+      res.send(result.toString());
+    });
+  }
+  
