cebb4695850a198eaaa5e584c75f09a34c66247d
anissavaughn <anissa.vaughnn@gmail.com>
2025-06-17 21:37:19 -0400

    update

--- index.js
+++ index.js
@@ -13,17 +13,6 @@ import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
 
-
-console.log({
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  SESSION_SECRET: process.env.SESSION_SECRET,
-  NETLIFY_URL: process.env.NETLIFY_URL,
-  NODE_SERVER_DOMAIN: process.env.NODE_SERVER_DOMAIN,
-});
-
-
-
 const app = express()
 
 
